ff93dcffcdd16393943cb870d546c461a7b7406d
antwain84 <61863225+antwain84@users.noreply.github.com>
2024-06-10 23:14:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,29 +2,30 @@
 #
 # creates Windows vm using the variables.tf and provider.tf
 #
-#
-# 
-#
 ########################################################
 
 resource "random_string" "sub_name" {
-
     length = 4
     min_lower = 2
     min_numeric = 2
 }
 
 resource "random_string" "sub_name2" {
-
     length = 4
     min_lower = 2
     min_numeric = 2
 }
 
 resource "azurerm_resource_group" "DevSecOps" {
-    name = "${var.business_unit}-DevSecOps"
+    name = "${var.business_unit}-DevSecOps-${random_string.sub_name2.id}"
     location = var.resource_group_location
+}
 
+resource "azurerm_resource_group" "test2" {
+    for_each = var.group_name_4each
+    name = "${var.business_unit}-${each.value}"
+    #name = "${var.business_unit}-${each.value}-${random_string.sub_name2.id}"  #WORKS: random_string.sub_name2.id creates only 1 unique ID and it's used on all RGs created
+    location = var.resource_group_location
 }
 
 
@@ -32,9 +33,9 @@ resource "azurerm_windows_virtual_machine" "win-test-02" {
   name                = "${var.business_unit}-${random_string.sub_name.id}-${random_string.sub_name2.id}"
   resource_group_name = azurerm_resource_group.DevSecOps.name
   location            = var.resource_group_location
-  size                = "Standard_B2als_v2"
-  admin_username      = "admin84"
-  admin_password      = "MyAdmin@12"
+  size                = "Standard_D1_v2" #D2lds_v5
+  admin_username      = "admin"
+  admin_password      = "MyAdmin@15"
   network_interface_ids = [
     azurerm_network_interface.win-test-01480_z2.id,
   ]
@@ -64,8 +65,8 @@ resource "azurerm_network_interface" "win-test-01480_z2" {
   }
 }
 
-resource "azurerm_virtual_network" "win-test-01-vnet" {
-  name                = "win-test-01-vnet"
+resource "azurerm_virtual_network" "win-test-0145AS84-vnet" {
+  name                = "win-test-0145AS84-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.DevSecOps.name
@@ -74,6 +75,6 @@ resource "azurerm_virtual_network" "win-test-01-vnet" {
 resource "azurerm_subnet" "default" {
   name                 = "default"
   resource_group_name  = azurerm_resource_group.DevSecOps.name
-  virtual_network_name = azurerm_virtual_network.win-test-01-vnet.name
+  virtual_network_name = azurerm_virtual_network.win-test-0145AS84-vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
