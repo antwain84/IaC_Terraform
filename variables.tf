8ef2d2fa49eb91f2f40d5f87ae59774d62318f96
antwain84 <61863225+antwain84@users.noreply.github.com>
2024-06-10 23:07:24 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,26 @@
 variable "resource_group_location" {
-    default = "southCentralus"
+    #default = "southCentralus"
+    default = "East us 2"
     description = "location of the resource group"
 }
 
 variable "resource_group_name_prefix" {
     default = "soc" # was "rg"
     description = "prefix of the resource group"
-
 }
 
 variable "business_unit" {
     default = "soc"
+}
+
+variable "group_name"{
+    default = ["group1", "group2", "group3"]
+    type = list(string)
+    description = "RG names"
+}
+
+variable "group_name_4each"{
+    default = ["DEV", "UAT", "SIT"]
+    type = set(string)
+    description = "RG names"
 }
