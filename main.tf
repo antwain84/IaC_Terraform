6d3ef2576459f84ce98624f738c5146d916bb6a2
Antwain Salisbury <61863225+antwain84@users.noreply.github.com>
2024-12-16 12:32:36 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,3 +113,29 @@ resource "azurerm_subnet" "default" {
   virtual_network_name = azurerm_virtual_network.win-test-0145AS84-vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.DevSecOps.name
+}
+
+resource "azurerm_network_security_rule" "example" {
+  name                        = "allow-rdp"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.DevSecOps.name
+  allocation_method   = "Dynamic"
+}
